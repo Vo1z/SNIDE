13f597ec010a7d7747ee99ab31ac4645151f07dc
Victor <vitya.voody@gmail.com>
2020-07-05 16:12:19 +0300

    initial commit for voiz_branch

--- SNIDE_project/src/Main.java
+++ SNIDE_project/src/Main.java
@@ -1,15 +1,21 @@
+import LaunchWindow.Launch;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application
 {
-    public static void main(String[] args) {
+    private Launch launchScene;
+
+    public static void main(String[] args)
+    {
         launch();
     }
+
     @Override
     public void init() throws Exception
     {
         System.out.println("On init"); //fixme
+        this.launchScene = new Launch();
     }
 
     @Override
@@ -17,6 +23,7 @@ public class Main extends Application
     {
         System.out.println("On start"); //fixme
 
+        stage.setScene(launchScene.getLaunchScene());
         stage.show();
     }
 
